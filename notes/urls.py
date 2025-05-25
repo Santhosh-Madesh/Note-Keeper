159998b267591b82f6b01d483e8006b1f686f7da
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-25 21:52:41 +0530

    Made urls which also gets data for the views update and delete

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index,create
+from .views import index,create,delete,update
 
 urlpatterns=[
     path("",index,name="index"),
-    path("create/",create,name="create")
+    path("create/",create,name="create"),
+    path("delete/<int:pk>/",delete,name="delete"),
+    path("update/<int:pk>/",update,name="update"),
 ]
