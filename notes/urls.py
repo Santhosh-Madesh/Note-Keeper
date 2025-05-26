6e96bd0621fbf43db042e64c564ad7791b3bd94a
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-26 10:36:26 +0530

    Established urls for the delete, update and the about view

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index,create,delete,update
+from .views import index,create,delete,update,about
 
 urlpatterns=[
     path("",index,name="index"),
     path("create/",create,name="create"),
     path("delete/<int:pk>/",delete,name="delete"),
     path("update/<int:pk>/",update,name="update"),
+    path("about/",about,name="about")
 ]
