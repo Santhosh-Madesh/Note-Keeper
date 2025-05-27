dc5a2b913906c42df210a226b833712b45c5ab5c
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-27 21:29:48 +0530

    Added a urlpattern for the login view from auth file

--- notes/urls.py
+++ notes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import index,create,delete,update,about
+from . import auth
 
 urlpatterns=[
     path("",index,name="index"),
     path("create/",create,name="create"),
     path("delete/<int:pk>/",delete,name="delete"),
     path("update/<int:pk>/",update,name="update"),
-    path("about/",about,name="about")
+    path("about/",about,name="about"),
+    path("login/",auth.login,name="login"),
 ]
