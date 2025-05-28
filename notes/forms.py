dbe94e8e75f953b59626f1524574fe9d6537b7c4
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-28 13:47:57 +0530

    Initial commit

--- notes/forms.py
+++ notes/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class NoteForm(forms.Form):
     title = forms.CharField(max_length=50)
     content = forms.CharField(widget=forms.Textarea())
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
